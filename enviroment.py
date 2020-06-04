6cff727de746d990e13efbae42bd24226fa06ac7
Kirill Shmilovich <kshmilov@gmail.com>
2020-06-04 18:15:42 -0500

    f

--- enviroment.py
+++ enviroment.py
@@ -1,11 +1,11 @@
-from simulation import LJSimualtion
+from simulation import LJSimualtion, ADPSimualtion
 from simtk import unit as u
 import numpy as np
 from gym import spaces
 import gym
 
 
-class SimEnv(gym.Env):
+class LJSimEnv(gym.Env):
     """Custom Environment that follows gym interface"""
 
     metadata = {"render.modes": ["human"]}
@@ -19,7 +19,7 @@ class SimEnv(gym.Env):
         report=False,
         **kwargs
     ):
-        super(SimEnv, self).__init__()
+        super(LJSimEnv, self).__init__()
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete actions:
@@ -99,9 +99,102 @@ class SimEnv(gym.Env):
         pass
 
 
+class ADPSimEnv(gym.Env):
+    """Custom Environment that follows gym interface"""
+
+    metadata = {"render.modes": ["human"]}
+
+    def __init__(
+        self,
+        min_temp=1 * u.kelvin,
+        max_temp=500 * u.kelvin,
+        delta=5 * u.kelvin,
+        episode_duration=1000,
+        report=False,
+        **kwargs
+    ):
+        super(ADPSimEnv, self).__init__()
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        self.min_temp = min_temp
+        self.max_temp = max_temp
+        self.delta = delta
+        self.episode_duration = episode_duration
+        self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
+        # Example for using image as input:
+        self.observation_space = spaces.Box(
+            low=0.0, high=1.0, shape=(232,), dtype=np.float32
+        )
+
+        self.reset(report=report)
+
+    def step(self, action):
+        n_steps = 500
+
+        # Change temp
+        deltaT = self.delta * action[0]
+        new_temp = self._temp_clamp(self.sim.temperature + deltaT)
+        self.sim.changeTemp(new_temp)
+
+        # Run sim and update episode step
+        self.sim.sim(n_steps)
+        self.episode_step += 1
+
+        # Get the state
+        observation = self._state()
+
+        # Calculate reward
+        rg = self.sim.rg()
+        reward = -1 * rg
+        info = {}
+        self.info_dict = {
+            "rg": rg,
+            "temperature": self.sim.temperature,
+            "reward": reward,
+        }
+
+        # Check if done
+        if self.episode_step >= self.episode_duration:
+            done = 1
+        else:
+            done = 0
+
+        return observation, reward, done, info
+
+    def _temp_clamp(self, x):
+        return max(self.min_temp, min(self.max_temp, x))
+
+    def _scale(self, x):
+        z = (x - self.min_temp) / (self.max_temp - self.min_temp)
+        return z
+
+    def _state(self):
+        state = np.array([self._scale(self.sim.temperature)])
+        # state = np.concatenate((state, self.sim.graphlet_features()))
+        state = np.concatenate((state, self.sim.pairwise_features()))
+        return state
+
+    def reset(self, report=False):
+        temp = (
+            np.random.uniform(low=self.min_temp._value, high=self.max_temp._value)
+            * self.min_temp.unit
+        )
+        self.sim = ADPSimualtion(report=report, temperature=temp)
+        observation = self._state()
+        self.episode_step = 0
+        return observation  # reward, done, info can't be included
+
+    def render(self, mode="human"):
+        pass
+
+    def close(self):
+        pass
+
+
 if __name__ == "main":
     from stable_baselines3.common.env_checker import check_env
 
-    env = SimEnv()
+    env = ADPSimEnv()
     # If the environment don't follow the interface, an error will be thrown
     check_env(env, warn=True)
--- simulation.py
+++ simulation.py
@@ -9,22 +9,21 @@ from utils import get_n_clusters
 
 from scipy.spatial.distance import pdist
 from graphlets.utils import pbc
+import mdtraj as md
 
 
 class LJSimualtion:
     def __init__(
-            self,
-            n_particles=100,
-            pressure=80 * u.atmosphere,
-            temperature=120 * u.kelvin,
-            timestep=2.5 * u.femtoseconds,
-            collision_rate=5.0 / u.picoseconds,
-            sigma=3.4 * u.angstrom,
-            epsilon=0.238 * u.kilocalories_per_mole,
-            report=False,
+        self,
+        n_particles=100,
+        temperature=120 * u.kelvin,
+        timestep=2.5 * u.femtoseconds,
+        collision_rate=5.0 / u.picoseconds,
+        sigma=3.4 * u.angstrom,
+        epsilon=0.238 * u.kilocalories_per_mole,
+        report=False,
     ):
         self.n_particles = n_particles
-        self.pressure = pressure
         self.temperature = temperature
         self.collision_rate = collision_rate
         self.timestep = timestep
@@ -42,17 +41,19 @@ class LJSimualtion:
 
     @property
     def xyz(self):
-        return (self.simulation.context.getState(
-            getPositions=True,
-            enforcePeriodicBox=True).getPositions(asNumpy=True)._value)  # nm
+        return (
+            self.simulation.context.getState(getPositions=True, enforcePeriodicBox=True)
+            .getPositions(asNumpy=True)
+            ._value
+        )  # nm
 
     @property
     def dims(self):
         return np.diag(
-            self.simulation.context.getState(
-                getPositions=True,
-                enforcePeriodicBox=True).getPeriodicBoxVectors(
-                    asNumpy=True)._value)  # nm
+            self.simulation.context.getState(getPositions=True, enforcePeriodicBox=True)
+            .getPeriodicBoxVectors(asNumpy=True)
+            ._value
+        )  # nm
 
     def graphlet_features(self, r_cut=None):
         if r_cut is None:
@@ -77,40 +78,37 @@ class LJSimualtion:
         return platform, properties
 
     def _setup(self, report=False):
-        self.fluid = LennardJonesFluid(nparticles=self.n_particles,
-                                       sigma=self.sigma,
-                                       epsilon=self.epsilon)
+        self.fluid = LennardJonesFluid(
+            nparticles=self.n_particles, sigma=self.sigma, epsilon=self.epsilon
+        )
         self.system = self.fluid.system
-        self.integrator = mm.LangevinIntegrator(self.temperature,
-                                                self.collision_rate,
-                                                self.timestep)
+        self.integrator = mm.LangevinIntegrator(
+            self.temperature, self.collision_rate, self.timestep
+        )
         platform, properties = self._get_platform()
-        self.simulation = mm.app.Simulation(self.fluid.topology, self.system,
-                                            self.integrator, platform,
-                                            properties)
+        self.simulation = mm.app.Simulation(
+            self.fluid.topology, self.system, self.integrator, platform, properties
+        )
         self.simulation.context.setPositions(self.fluid.positions)
 
         self.simulation.minimizeEnergy()
         self.simulation.context.setVelocitiesToTemperature(self.temperature)
         if report:
             self.simulation.reporters.append(
-                mm.app.StateDataReporter(stdout,
-                                         100,
-                                         step=True,
-                                         temperature=True,
-                                         separator=" | "))
+                mm.app.StateDataReporter(
+                    stdout, 100, step=True, temperature=True, separator=" | "
+                )
+            )
 
 
 class ADPSimualtion:
     def __init__(
-            self,
-            pressure=80 * u.atmosphere,
-            temperature=120 * u.kelvin,
-            timestep=2.5 * u.femtoseconds,
-            collision_rate=5.0 / u.picoseconds,
-            report=False,
+        self,
+        temperature=300 * u.kelvin,
+        timestep=2.0 * u.femtoseconds,
+        collision_rate=1.0 / u.picoseconds,
+        report=False,
     ):
-        self.pressure = pressure
         self.temperature = temperature
         self.collision_rate = collision_rate
         self.timestep = timestep
@@ -126,17 +124,33 @@ class ADPSimualtion:
 
     @property
     def xyz(self):
-        return (self.simulation.context.getState(
-            getPositions=True,
-            enforcePeriodicBox=True).getPositions(asNumpy=True)._value)  # nm
+        return (
+            self.simulation.context.getState(getPositions=True, enforcePeriodicBox=True)
+            .getPositions(asNumpy=True)
+            ._value
+        )  # nm
 
     @property
     def dims(self):
         return np.diag(
-            self.simulation.context.getState(
-                getPositions=True,
-                enforcePeriodicBox=True).getPeriodicBoxVectors(
-                    asNumpy=True)._value)  # nm
+            self.simulation.context.getState(getPositions=True, enforcePeriodicBox=True)
+            .getPeriodicBoxVectors(asNumpy=True)
+            ._value
+        )  # nm
+
+    def pairwise_features(self):
+        xyz = self.xyz
+        dims = self.dims
+        d_max = np.sqrt(np.square(dims / 2.0).sum())
+        pairwise_dists = pdist(xyz, metric=pbc, dims=dims) / d_max
+        return pairwise_dists
+
+    def rg(self):
+        trj = md.Trajectory(
+            self.xyz, self.ADP.mdtraj_topology, unitcell_lengths=self.dims
+        )
+        rg = md.compute_rg(trj)[0]
+        return rg
 
     def _get_platform(self):
         platform = mm.Platform.getPlatformByName("OpenCL")
@@ -146,26 +160,27 @@ class ADPSimualtion:
     def _setup(self, report=False):
         self.ADP = AlanineDipeptideImplicit()
         self.system = self.ADP.system
-        self.integrator = mm.LangevinIntegrator(self.temperature,
-                                                self.collision_rate,
-                                                self.timestep)
+        self.integrator = mm.LangevinIntegrator(
+            self.temperature, self.collision_rate, self.timestep
+        )
         platform, properties = self._get_platform()
-        self.simulation = mm.app.Simulation(self.ADP.topology, self.system,
-                                            self.integrator, platform,
-                                            properties)
+        self.simulation = mm.app.Simulation(
+            self.ADP.topology, self.system, self.integrator, platform, properties
+        )
         self.simulation.context.setPositions(self.ADP.positions)
 
         self.simulation.minimizeEnergy()
         self.simulation.context.setVelocitiesToTemperature(self.temperature)
         if report:
             self.simulation.reporters.append(
-                mm.app.StateDataReporter(stdout,
-                                         100,
-                                         step=True,
-                                         temperature=True,
-                                         separator=" | "))
+                mm.app.StateDataReporter(
+                    stdout, 100, step=True, temperature=True, separator=" | "
+                )
+            )
 
 
 if __name__ == "__main__":
-    env = ADPSimualtion()
-    ADPSimualtion.sim(1000)
+    env = ADPSimualtion(report=True)
+    env.sim(1000)
+    env.rg()
+    print(env.pairwise_features().shape)
